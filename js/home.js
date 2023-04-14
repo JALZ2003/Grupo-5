759a4a5cd0c8733e3f3fb56b7515213b75977e26
santivanegas16 <santiago.vanegasm16@gmail.com>
2023-04-14 16:23:45 -0500

    Avance checkbox

--- js/home.js
+++ js/home.js
@@ -2,6 +2,7 @@
 const container = document.querySelector('.swiper-wrapper');
 
 function addHomeEvents() {
+    container.innerHTML = '';
     for (let i = 0; i < eventos.eventos.length; i++) {
         const slide = document.createElement('div');
         slide.classList.add("swiper-slide");
@@ -10,7 +11,7 @@ function addHomeEvents() {
         let CardDescription = eventos.eventos[i].description;
         let CardPrice = eventos.eventos[i].price;
         const homeEvents = createCardDiv(CardImage, CardName, CardDescription, CardPrice,
-            eventos.eventos[i].date, eventos.eventos[i].category, eventos.eventos[i].place, eventos.eventos[i].capacity, 
+            eventos.eventos[i].date, eventos.eventos[i].category, eventos.eventos[i].place, eventos.eventos[i].capacity,
             eventos.eventos[i].assistance || eventos.eventos[i].estimate);
         slide.appendChild(homeEvents);
         container.appendChild(slide);
@@ -35,3 +36,58 @@ var swiper = new Swiper(".slide-content", {
         950: { slidesPerView: 4, slidesPerGroup: 4 },
     },
 });
+
+const categoriasEventos = [...new Set(eventos.eventos.map(r => r.category))]
+
+categoriasEventos.forEach(r => {
+    const checksHome = document.getElementById("checksHome")
+    let check = document.createElement('div');
+    check.className = "form-check form-check-inline"
+    check.innerHTML = `
+    <input class="form-check-input" type="checkbox" id="${r}" value="${r}">
+    <label class="form-check-label" for="${r}">${r}</label>
+          `
+    checksHome.appendChild(check)
+});
+
+
+let soloEventos = eventos.eventos
+const allChecks = document.querySelectorAll(".form-check-input")
+
+allChecks.forEach(e => {
+    let check = document.getElementById(e.id)
+        
+    check.addEventListener("change", function () {
+    
+        if(check.checked) {
+            eventos.eventos = filtroCheck(eventos.eventos, e.id)
+            addHomeEvents();
+            console.log("Se activó")
+          } else {
+            eventos.eventos = soloEventos
+            addHomeEvents();
+            console.log("Se desactivó")
+          }
+    })
+    console.log(e.id)
+})
+
+function filtroCheck(array,categoria) {
+    return array.filter(n=> n.category == categoria)
+}
+
+// let check1 = document.getElementById("Food Fair")
+
+// check1.addEventListener("change", function () {
+    
+//     if(check1.checked) {
+//         eventos.eventos = filtroCheck(eventos.eventos,"Food Fair")
+//         addHomeEvents();
+//         console.log("Se activó")
+//       } else {
+//         eventos.eventos = soloEventos
+//         addHomeEvents();
+//         console.log("Se desactivó")
+//       }
+// })
+
