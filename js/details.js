d98464d7354c9c304c8480d0c38db1a627b47a18
santivanegas16 <santiago.vanegasm16@gmail.com>
2023-04-21 00:06:14 -0500

    se corrige assitance o estimate

--- js/details.js
+++ js/details.js
@@ -1,19 +1,24 @@
+// usando Fetch
+
 // let id = JSON.parse(sessionStorage.getItem('id'));
-let id = "6351b0a1b82050da15b3a866"
+let id = "6351b0a1b82050da15b3a883"
 let url = "https://pro-talento.up.railway.app/api/amazing/" + id;
 
 async function fetchApi(url) {
     try {
         let response = await fetch(url); //fetcheo la API
         response = await response.json(); //Transformo la API en datos que pueda usar
-        let name = response.response.name;
-        let category = response.response.category;
-        let price = response.response.price;
-        let {image, description, date, assistance, capacity, place} = response.response  // con esto podemos hacer todo lo de las lineas anteriores resumido
-        setDetails(image,name,date,description,category,place,capacity,assistance,price) 
+        let { image, name, category, price, description, date, assistance, capacity, place } = response.response  // con esto podemos hacer todo lo de las lineas anteriores resumido
+        if ("assistance" in response.response) {
+            assistance = "Assistance: " + assistance;
+        } else {
+            assistance = "Estimate: " + response.response.estimate;
+        }
+        setDetails(image, name, date, description, category, place, capacity, assistance, price)
     } catch (error) {
         console.log(error)
     }
 }
 
 fetchApi(url);
+
